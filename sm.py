da3ef1e762b0d3ff33b9ffde93f0d46a67c881e3
Darkhan Tastanov <75112576+Nakhrad@users.noreply.github.com>
2023-02-03 17:42:36 +0600

    Delete sm.py

--- sm.py
+++
@@ -1,9 +0,0 @@
-x=1
-months = {"Jan": [x while x in range(32) if x <= 31
-                    x=x+1],
-          "Feb": [x for x in range(32) if x <= 30],
-        }
-print(months)
-a = int(input("Day:"))
-v = months.find(a)
-print(v)
