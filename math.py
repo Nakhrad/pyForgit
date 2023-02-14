da0e51195f844582919ea46f74c60aafd5afd54b
NakhraD <tastanovd20@gmail.com>
2023-02-14 15:51:16 +0600

    lab4math

---
+++ math.py
@@ -0,0 +1,17 @@
+import math
+#degree to rad
+# def rad(d):
+#     return math.radians(d)
+# print(rad(15))
+#areafotrapezoid
+# def trap(h, fv, sv):
+#     return ((fv + sv)/2) * h
+# print(trap(5, 5, 6))
+#areaofpoly
+# def poly(ns, l):
+#     return ns * ((l * l/2)/2)
+# print("The area of the polygon is:", int(poly(4, 25)))
+#areaofpole
+# def pole(l, h):
+#     return l * h
+# print(float(pole(5, 6)))
