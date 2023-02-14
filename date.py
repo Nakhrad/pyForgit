b92aa28f00aeb7486b464f51e76917f9cace12c9
NakhraD <tastanovd20@gmail.com>
2023-02-14 20:33:20 +0600

    lab4date

---
+++ date.py
@@ -0,0 +1,27 @@
+#fivefromtod
+# from datetime import date, timedelta
+#
+# datefive = date.today() + timedelta(5)
+# print('Current Date :',date.today())
+# print('5 days from Current Date :',datefive)
+#YTT
+# from datetime import date, timedelta
+# y = date.today() - timedelta(1)
+# t = date.today()
+# to = date.today() + timedelta(1)
+# print("Yesterday:", y)
+# print("Today:", t)
+# print("Tomorrow:", to)
+#mics
+# import datetime
+# dt = datetime.datetime.today().replace(microsecond=0)
+# print(dt)
+#diftd
+# from datetime import timedelta, date
+# def date_diff_in_Seconds(dt2, dt1):
+#   timedelta = dt2 - dt1
+#   return timedelta.days * 24 * 3600 + timedelta.seconds
+# d = int(input("Day after today:"))
+# date1 = date.today()
+# date2 = date.today() + timedelta(d)
+# print("\n%d seconds" %(date_diff_in_Seconds(date2, date1)))
